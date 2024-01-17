d62cd674b1796f34b096a5d789240f8c70150ccf
aburahan8721 <aburaihan8721@gmail.com>
2024-01-17 17:52:28 +0600

    add fake data

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: "1",
+    name: "GoPro",
+    price: 57,
+    quantity: 0,
+  },
+  {
+    id: "2",
+    name: "Tripod",
+    price: 7.99,
+    quantity: 0,
+  },
+  {
+    id: "3",
+    name: "Bag",
+    price: 4.99,
+    quantity: 0,
+  },
+];
